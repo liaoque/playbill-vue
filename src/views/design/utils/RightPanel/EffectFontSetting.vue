124d6ff28f861bc1a5d22aafd24fd134f0014653
mzq <mazuqiang@lane.com>
2022-10-10 17:35:07 +0800

    字体文件

--- src/views/design/utils/RightPanel/EffectFontSetting.vue
+++ src/views/design/utils/RightPanel/EffectFontSetting.vue
@@ -40,6 +40,8 @@
   } from 'vue'
   import FontFaceObserver from 'fontfaceobserver'
 
+
+
   const props = defineProps({
     klassObj: Object
   })
@@ -77,9 +79,70 @@
 
 </script>
 
-<style>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
+<style scoped>
+
+
+  @font-face {
+    font-family: observer-test1;
+    src: url("./assets/fonts/sourcesanspro-regular.woff") format('woff'),
+    url("./assets/fonts/sourcesanspro-regular.ttf") format('truetype');
+  }
+
+  @font-face {
+    font-family: unknown;
+    src: url(unknown.woff) format('woff'),
+    url(unknown.ttf) format('truetype');
+  }
+
+  @font-face {
+    font-family: observer-test3;
+    src: url("~@/assets/fonts/sourcesanspro-regular.woff") format('woff'),
+    url("~@/assets/fonts/sourcesanspro-regular.ttf") format('truetype');
+  }
+
+  @font-face {
+    font-family: observer-test4;
+    src: url("~@/assets/fonts/subset.woff") format('woff'),
+    url("~@/assets/fonts/subset.ttf") format('truetype');
+    unicode-range: u+0021;
+  }
+
+  @font-face {
+    font-family: observer-test5;
+    src: url("~@/assets/fonts/subset.woff") format('woff'),
+    url("~@/assets/fonts/subset.ttf") format('truetype');
+    unicode-range: u+4 e2d, u+56 fd;
+  }
+
+  @font-face {
+    font-family: observer-test6;
+    src: url("~@/assets/fonts/subset.woff") format('woff'),
+    url("~@/assets/fonts/subset.ttf") format('truetype');
+    unicode-range: u+10 ffff;
+  }
+
+  @font-face {
+    font-family: observer-test7;
+    src: url("~@/assets/fonts/subset.woff") format('woff'),
+    url("~@/assets/fonts/subset.ttf") format('truetype');
+    unicode-range: u+23;
+  }
+
+  @font-face {
+    font-family: observer-test8;
+    src: url(@/assets/fonts/sourcesanspro-regular.woff) format('woff'),
+    url(@/assets/fonts/sourcesanspro-regular.ttf) format('truetype');
+  }
+
+  @font-face {
+    font-family: Trebuchet W01 Regular;
+    src: url(@/assets/fonts/sourcesanspro-regular.woff) format('woff'),
+    url(@/assets/fonts/sourcesanspro-regular.ttf) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Neue Frutiger 1450 W04';
+    src: url(@/assets/fonts/sourcesanspro-regular.woff) format('woff'),
+    url(@/assets/fonts/sourcesanspro-regular.ttf) format('truetype');
   }
 </style>
