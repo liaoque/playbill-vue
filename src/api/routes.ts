36923e98ddbf8ae32694a0bb0b9aad211c28fbbd
mzq <mazuqiang@lane.com>
2022-12-06 09:49:48 +0800

    调整接口

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -6,30 +6,30 @@ type Result = {
 };
 
 export const getAsyncRoutes = (params?: object) => {
-  return http.request<Result>("get", "/getAsyncRoutes", {params});
+  return http.request<Result>("get", "/api/getAsyncRoutes", {params});
 };
 
 export const Login = (params?: object) => {
-  return http.post("/login", {data: params});
+  return http.post("/api/login", {data: params});
 };
 
 export const PlaybillView = (params?: object) => {
-  return http.post("/playbill/view", {data: params});
+  return http.post("/api/playbill/view", {data: params});
 };
 
 
 export const PlaybillSave = (params?: object) => {
-  return http.post("/playbill/save", {data: params});
+  return http.post("/api/playbill/save", {data: params});
 };
 
 
 export const PlaybillQuery = (id: string) => {
-  return http.get("/playbill/query/id/" + id);
+  return http.get("/api/playbill/query/id/" + id);
 };
 
 
 export const PlaybillAll = (params?: object) => {
-  return http.get("/playbill/list", {data: params});
+  return http.get("/api/playbill/list", {data: params});
 };
 
 export const apiBaseUrl = () => {
